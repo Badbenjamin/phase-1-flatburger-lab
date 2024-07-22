8f099c5996ee284e5673608871d29551492a36ae
ben mccarthy <bmccarthy90@gmail.com>
2024-07-22 17:42:42 -0400

    flatburger lab more documentation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@
 // Click event is fired and displayBurgerDetails is invoked
 // the info burger contains is different depending on which element is clicked
 const displayBurgerDetails = (burger) => {
+    
+    // find element that will display image
     const imageElement = document.getElementById('image');
+    // set src to image property of burger object
     imageElement.src = burger.image
 
     const burgerName = document.getElementById('name')
@@ -15,16 +18,19 @@ const displayBurgerDetails = (burger) => {
 };
 
 const addToCart = () => {
-  
+    // find the form   
    const addToCartForm = document.getElementById('add-to-cart-form')
+    // attatch an event listener to the form   
    addToCartForm.addEventListener('submit', (event) => {
     event.preventDefault();
     
+    // this is the elment that our form submission number will be added to.
     const numberInCartCountElement = document.getElementById('number-in-cart-count')
-    const numberToAddElement = document.getElementById('number-to-add');
-    
-    numberInCartCountElement.textContent = Number(numberToAddElement.value) + Number(numberInCartCountElement.textContent);
-    
+    // input element that has id 'number-to-add' numberToAddElement.value will return the form input
+    const numberToAddInputElement = document.getElementById('number-to-add');
+    // console.log(numberInCartCountElement.textContent)
+    numberInCartCountElement.textContent = Number(numberToAddInputElement.value) + Number(numberInCartCountElement.textContent);
+    console.log(numberInCartCountElement.textContent)
 
 
 
@@ -46,16 +52,30 @@ const addBurgerNamesToMenu = () => {
             const spanElement = document.createElement('span');
             spanElement.textContent = burger.name
             // add the span element to the restarauntMenu div
-            restarauntMenu.appendChild(spanElement);
+            // restarauntMenu.appendChild(spanElement);
+
+            // DELETE BUTTON 
+            const divElement = document.createElement('div')
+            const deleteButton = document.createElement('button')
+            deleteButton.textContent = "Delete"
+
+            divElement.appendChild(spanElement)
+            divElement.appendChild(deleteButton)
+            restarauntMenu.appendChild(divElement);
+
+            deleteButton.addEventListener("click", () => {
+                divElement.remove();
+            })
         
             // add an event listener to each span element
             spanElement.addEventListener("click", () => {
                 // when fired, the displayBurgerDetails function fires, and it has been populated with 
                 // the information from the individual burger object
                 displayBurgerDetails(burger);
-                console.log("burger", burger)
             });
     })
+    // this code grabs the first burger in our json object and sets it as the default display when the page loads
+    displayBurgerDetails(burgers[0]);
 });
 }
 
